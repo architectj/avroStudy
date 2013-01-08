616e8e2715b1a8890d41c8a9bd8244355b315693
TeaJune <kiora1120@gmail.com>
2013-01-08 11:22:35 +0900

    Test Code

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+/**
+ * A class representing a window on the screen.
+ * For example:
+ * <pre>
+ *    Window win = new Window(parent);
+ *    win.show();
+ * </pre>
+ *
+ * @author <A HREF="mailto:[kiora1120@gmail.com]">TJune Kim</A>
+ * @version 1.0
+ */
+public class Main {
+}
